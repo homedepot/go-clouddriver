d13df3611f910a1b4c81f7f89583b6774d4bb526
Billy Bolton <billy@bolton.email>
2020-08-24 09:45:42 -0400

    list namespaces in parallel when 'expand=true' on credentials list

--- pkg/http/core/credentials.go
+++ pkg/http/core/credentials.go
@@ -1,11 +1,19 @@
 package core
 
 import (
+	"encoding/base64"
+	"log"
 	"net/http"
+	"strings"
+	"sync"
 
 	clouddriver "github.com/billiford/go-clouddriver/pkg"
+	"github.com/billiford/go-clouddriver/pkg/kubernetes"
 	"github.com/billiford/go-clouddriver/pkg/sql"
 	"github.com/gin-gonic/gin"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/rest"
 )
 
 // I'm not sure why spinnaker needs this, but without it several necessary Spinnaker manifest stages are missing.
@@ -49,6 +57,7 @@ var spinnakerKindMap = map[string]string{
 func ListCredentials(c *gin.Context) {
 	expand := c.Query("expand")
 	sc := sql.Instance(c)
+	kc := kubernetes.Instance(c)
 	credentials := []clouddriver.Credential{}
 
 	providers, err := sc.ListKubernetesProviders()
@@ -71,11 +80,14 @@ func ListCredentials(c *gin.Context) {
 		}
 
 		sca := clouddriver.Credential{
-			AccountType:                 provider.Name,
-			ChallengeDestructiveActions: false,
-			CloudProvider:               "kubernetes",
-			Environment:                 provider.Name,
-			Name:                        provider.Name,
+			AccountType: provider.Name,
+			// CacheThreads:                0,
+			// ChallengeDestructiveActions: false,
+			CloudProvider: "kubernetes",
+			// DockerRegistries:            nil,
+			// Enabled:                     false,
+			Environment: provider.Name,
+			Name:        provider.Name,
 			Permissions: clouddriver.Permissions{
 				READ:  readGroups,
 				WRITE: writeGroups,
@@ -84,7 +96,9 @@ func ListCredentials(c *gin.Context) {
 			ProviderVersion:         "v2",
 			RequiredGroupMembership: []interface{}{},
 			Skin:                    "v2",
-			// SpinnakerKindMap:        spinnakerKindMap,
+			// SpinnakerKindMap: map[string]string{
+			// 	"": "",
+			// },
 			Type: "kubernetes",
 		}
 
@@ -92,6 +106,94 @@ func ListCredentials(c *gin.Context) {
 			sca.SpinnakerKindMap = spinnakerKindMap
 		}
 		credentials = append(credentials, sca)
+	}
+
+	type AccountNamespaces struct {
+		Name       string
+		Namespaces []string
+	}
+
+	// Only list namespaces when the 'expand' query param is set to true.
+	//
+	// Gate is polling the endpoint `/credentials?expand=true` once every
+	// thirty seconds. Each gate instance is doing this, making the requests to get
+	// all provider's namespaces a multiple of how many gate instances there are.
+	if expand == "true" {
+		wg := &sync.WaitGroup{}
+		accountNamespacesCh := make(chan AccountNamespaces, len(providers))
+		wg.Add(len(providers))
+
+		// Get all namespaces of allowed accounts asynchronysly.
+		for _, provider := range providers {
+			go func(account string) {
+				defer wg.Done()
+
+				provider, err := sc.GetKubernetesProvider(account)
+				if err != nil {
+					log.Println("/credentials error getting provider:", err.Error())
+					return
+				}
+
+				cd, err := base64.StdEncoding.DecodeString(provider.CAData)
+				if err != nil {
+					log.Println("/credentials error decoding provider ca data:", err.Error())
+					return
+				}
+
+				config := &rest.Config{
+					Host:        provider.Host,
+					BearerToken: provider.BearerToken,
+					TLSClientConfig: rest.TLSClientConfig{
+						CAData: cd,
+					},
+				}
+
+				err = kc.SetDynamicClientForConfig(config)
+				if err != nil {
+					log.Println("/credentials error creating dynamic account:", err.Error())
+					return
+				}
+
+				gvr := schema.GroupVersionResource{
+					Group:    "",
+					Version:  "v1",
+					Resource: "namespaces",
+				}
+				// timeout listing namespaces to 5 seconds
+				timeout := int64(5)
+				result, err := kc.List(gvr, metav1.ListOptions{
+					TimeoutSeconds: &timeout,
+				})
+				if err != nil {
+					log.Println("/credentials error listing using kubernetes account:", err.Error())
+					return
+				}
+
+				namespaces := []string{}
+				for _, ns := range result.Items {
+					namespaces = append(namespaces, ns.GetName())
+				}
+				an := AccountNamespaces{
+					Name:       account,
+					Namespaces: namespaces,
+				}
+
+				accountNamespacesCh <- an
+			}(provider.Name)
+		}
+
+		wg.Wait()
+
+		close(accountNamespacesCh)
+
+		for an := range accountNamespacesCh {
+			for i, cred := range credentials {
+				if strings.EqualFold(an.Name, cred.Name) {
+					cred.Namespaces = an.Namespaces
+					credentials[i] = cred
+				}
+			}
+		}
 	}
 
 	c.JSON(http.StatusOK, credentials)
