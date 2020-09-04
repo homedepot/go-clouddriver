55452467e146773604e04d5207b7ef4a677ab77d
Billy <billy@bolton.email>
2020-09-04 14:58:34 -0400

    feat(artifacts): add helm client (#4)
    * add artifacts endpoint
    
    * add helm client

---
+++ pkg/helm/client.go
@@ -0,0 +1,126 @@
+package helm
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"sync"
+
+	"github.com/gin-gonic/gin"
+	"gopkg.in/yaml.v2"
+)
+
+const (
+	ClientInstanceKey = `HelmClient`
+)
+
+type Index struct {
+	APIVersion string                `json:"apiVersion" yaml:"apiVersion"`
+	Entries    map[string][]Resource `json:"entries" yaml:"entries"`
+}
+
+type Resource struct {
+	APIVersion string `json:"apiVersion" yaml:"apiVersion"`
+	AppVersion string `json:"appVersion" yaml:"appVersion"`
+	// Created     time.Time `json:"created"`
+	Description string `json:"description" yaml:"description"`
+	Digest      string `json:"digest" yaml:"digest"`
+	Home        string `json:"home" yaml:"home"`
+	// Maintainers []struct {
+	// 	Email string `json:"email"`
+	// 	Name  string `json:"name"`
+	// } `json:"maintainers"`
+	Name    string   `json:"name" yaml:"name"`
+	Urls    []string `json:"urls" yaml:"urls"`
+	Version string   `json:"version" yaml:"version"`
+}
+
+//go:generate counterfeiter . Client
+type Client interface {
+	GetIndex() (Index, error)
+	GetChart(string, string) ([]byte, error)
+}
+
+var (
+	etag  string
+	cache Index
+	mux   sync.Mutex
+)
+
+func NewClient(u string) Client {
+	return &client{u: u}
+}
+
+type client struct {
+	u string
+}
+
+func (c *client) GetIndex() (Index, error) {
+	i := Index{}
+
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("%s/index.yaml", c.u), nil)
+	if err != nil {
+		return i, err
+	}
+
+	req.Header.Add("If-None-Match", etag)
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return i, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode == http.StatusNotModified {
+		mux.Lock()
+		defer mux.Unlock()
+
+		return cache, nil
+	}
+
+	if res.StatusCode < 200 || res.StatusCode > 399 {
+		return i, errors.New("error getting helm index: " + res.Status)
+	}
+
+	b, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return i, err
+	}
+
+	err = yaml.Unmarshal(b, &i)
+	if err != nil {
+		return i, err
+	}
+
+	mux.Lock()
+	defer mux.Unlock()
+
+	cache = i
+	etag = res.Header.Get("etag")
+
+	return i, nil
+}
+
+func (c *client) GetChart(name, version string) ([]byte, error) {
+	res, err := http.Get(fmt.Sprintf("%s/%s-%s.tgz", c.u, name, version))
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode < 200 || res.StatusCode > 399 {
+		return nil, errors.New("error getting helm chart: " + res.Status)
+	}
+
+	b, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+func Instance(c *gin.Context) Client {
+	return c.MustGet(ClientInstanceKey).(Client)
+}
---
+++ pkg/helm/client_test.go
@@ -0,0 +1,211 @@
+package helm_test
+
+import (
+	"net/http"
+
+	"github.com/billiford/go-clouddriver/pkg/helm"
+	. "github.com/billiford/go-clouddriver/pkg/helm"
+	"github.com/gin-gonic/gin"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/ghttp"
+)
+
+var _ = Describe("Client", func() {
+	var (
+		server *ghttp.Server
+		client Client
+		err    error
+		index  helm.Index
+		b      []byte
+	)
+
+	BeforeEach(func() {
+		server = ghttp.NewServer()
+		client = NewClient(server.URL())
+	})
+
+	AfterEach(func() {
+		server.Close()
+	})
+
+	Describe("#GetIndex", func() {
+		JustBeforeEach(func() {
+			index, err = client.GetIndex()
+		})
+
+		When("the uri is invalid", func() {
+			BeforeEach(func() {
+				client = NewClient("::haha")
+			})
+
+			It("returns an error", func() {
+				Expect(err).ToNot(BeNil())
+			})
+		})
+
+		When("the server is not reachable", func() {
+			BeforeEach(func() {
+				server.Close()
+			})
+
+			It("returns an error", func() {
+				Expect(err).ToNot(BeNil())
+			})
+		})
+
+		When("the response is not 2XX", func() {
+			BeforeEach(func() {
+				server.AppendHandlers(
+					ghttp.RespondWith(http.StatusInternalServerError, nil),
+				)
+			})
+
+			It("returns an error", func() {
+				Expect(err).ToNot(BeNil())
+				Expect(err.Error()).To(Equal("error getting helm index: 500 Internal Server Error"))
+			})
+		})
+
+		When("the response is status not modified", func() {
+			BeforeEach(func() {
+				server.AppendHandlers(
+					ghttp.RespondWith(http.StatusNotModified, nil),
+				)
+			})
+
+			It("succeeds", func() {
+				Expect(err).To(BeNil())
+			})
+		})
+
+		When("the server returns bad data", func() {
+			BeforeEach(func() {
+				server.AppendHandlers(
+					ghttp.RespondWith(http.StatusOK, ";{["),
+				)
+			})
+
+			It("returns an error", func() {
+				Expect(err).ToNot(BeNil())
+				Expect(err.Error()).To(Equal("yaml: unmarshal errors:\n  line 1: cannot unmarshal !!str `;{[` into helm.Index"))
+			})
+		})
+
+		When("it succeeds", func() {
+			BeforeEach(func() {
+				server.AppendHandlers(ghttp.CombineHandlers(
+					ghttp.VerifyRequest(http.MethodGet, "/index.yaml"),
+					ghttp.RespondWith(http.StatusOK, `apiVersion: v1
+entries:
+  hello-app:
+  - apiVersion: v2
+    appVersion: "1.0"
+    created: 2020-03-20T13:13:42.956Z
+    description: A Helm chart for deploying hello-app to Kuberntes
+    digest: a934a39ba7f77e8e16b609d01c493d7ead5ec24c78d2918d3a00ebaa3c3fdfd2
+    home: https://pages.github.homedepot.com/cd
+    maintainers:
+    - email: ContinuousDeployment@homedepot.com
+      name: CD Team
+    name: hello-app
+    urls:
+    - https://helm.artifactory.homedepot.com/artifactory/helm/thd-cd-hello-app-1.0.0.tgz
+    version: 1.0.0
+  thd-cd-hello-app:
+  - apiVersion: v2
+    appVersion: "2.0"
+    created: 2020-08-14T22:11:19.894Z
+    description: A Helm chart for deploying hello-app to Kubernetes
+    digest: c9c2663958ffdf2c790ebfd0f502176c3ac663014e9d6ce49955c4178c2f43d7
+    home: https://pages.github.homedepot.com/cd
+    maintainers:
+    - email: ContinuousDeployment@homedepot.com
+      name: CD Team
+    name: thd-cd-hello-app
+    urls:
+    - https://helm.artifactory.homedepot.com/artifactory/helm/thd-cd-hello-app-3.0.2.tgz
+    version: 3.0.2`),
+				))
+			})
+
+			It("succeeds", func() {
+				Expect(err).To(BeNil())
+				Expect(index.Entries).To(HaveLen(2))
+			})
+		})
+	})
+
+	Describe("#GetChart", func() {
+		JustBeforeEach(func() {
+			b, err = client.GetChart("test-name", "0.1.0")
+		})
+
+		When("the uri is invalid", func() {
+			BeforeEach(func() {
+				client = NewClient("::haha")
+			})
+
+			It("returns an error", func() {
+				Expect(err).ToNot(BeNil())
+			})
+		})
+
+		When("the server is not reachable", func() {
+			BeforeEach(func() {
+				server.Close()
+			})
+
+			It("returns an error", func() {
+				Expect(err).ToNot(BeNil())
+			})
+		})
+
+		When("the response is not 2XX", func() {
+			BeforeEach(func() {
+				server.AppendHandlers(
+					ghttp.RespondWith(http.StatusInternalServerError, nil),
+				)
+			})
+
+			It("returns an error", func() {
+				Expect(err).ToNot(BeNil())
+				Expect(err.Error()).To(Equal("error getting helm chart: 500 Internal Server Error"))
+			})
+		})
+
+		When("it succeeds", func() {
+			BeforeEach(func() {
+				server.AppendHandlers(ghttp.CombineHandlers(
+					ghttp.VerifyRequest(http.MethodGet, "/test-name-0.1.0.tgz"),
+					ghttp.RespondWith(http.StatusOK, `some-binary-data`),
+				))
+			})
+
+			It("succeeds", func() {
+				Expect(err).To(BeNil())
+				Expect(string(b)).To(Equal("some-binary-data"))
+			})
+		})
+	})
+
+	Describe("#Instance", func() {
+		var c *gin.Context
+		var c2 Client
+
+		BeforeEach(func() {
+			c = &gin.Context{}
+			c.Set(ClientInstanceKey, client)
+		})
+
+		When("it succeeds", func() {
+			BeforeEach(func() {
+				c2 = Instance(c)
+			})
+
+			It("succeeds", func() {
+				Expect(c2).ToNot(BeNil())
+			})
+		})
+	})
+})
