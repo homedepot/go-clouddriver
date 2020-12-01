54dd5993e567cbeea4645db66d4933d7002f1b53
Edson H Philippe <edson_h_philippe@homedepot.com>
2020-12-01 17:42:50 -0500

    alphabetized properties

--- pkg/kubernetes_clusters.go
+++ pkg/kubernetes_clusters.go
@@ -1,13 +1,14 @@
 package clouddriver
 
 type kubernetes_clusters struct {
-	id string `json:"-" gorm:"primary_key"`
 	account_name string
+	cluster string
+	id string `json:"-" gorm:"primary_key"`
 	kind  string
 	name string
 	namespace string
 	spinnaker_app string
-	cluster string
+
 }
 
 func (kubernetes_clusters) TableName() string {
