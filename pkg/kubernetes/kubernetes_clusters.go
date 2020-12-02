fbcb41808d28b37072b4d0b63186f2d72ef74339
Edson H Philippe <edson_h_philippe@homedepot.com>
2020-12-02 13:44:11 -0500

    format using go fmt

--- pkg/kubernetes/kubernetes_clusters.go
+++ pkg/kubernetes/kubernetes_clusters.go
@@ -1,12 +1,12 @@
 package kubernetes
 
 type Cluster struct {
-	AccountName string
-	Cluster string
-	Id string `json:"-" gorm:"primary_key"`
-	Kind  string
-	Name string
-	Namespace string
+	AccountName  string
+	Cluster      string
+	Id           string `json:"-" gorm:"primary_key"`
+	Kind         string
+	Name         string
+	Namespace    string
 	SpinnakerApp string
 }
 
