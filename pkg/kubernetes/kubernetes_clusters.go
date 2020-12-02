c1fa71753424bb1ae10573be0e031c1958873356
Edson H Philippe <edson_h_philippe@homedepot.com>
2020-12-02 10:16:36 -0500

    made the struct exportable and changed the fields to camel case

---
+++ pkg/kubernetes/kubernetes_clusters.go
@@ -0,0 +1,15 @@
+package kubernetes
+
+type Cluster struct {
+	AccountName string
+	Cluster string
+	Id string `json:"-" gorm:"primary_key"`
+	Kind  string
+	Name string
+	Namespace string
+	SpinnakerApp string
+}
+
+func (Cluster) TableName() string {
+	return "kubernetes_clusters"
+}
