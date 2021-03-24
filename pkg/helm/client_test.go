004101effac3175ff065092afb69776904e70cb5
Billy <billy@bolton.email>
2021-03-24 15:44:32 -0400

    update helm client to use resource URLs (#23)
    * add base64 validation when onboarding kubernetes providers (#16)
    
    * add logic to sort manifests by priority (#17)
    
    * use a resources defined URLs when fetching a chart (#19)
    
    * use a resources defined URLs when fetching a chart
    
    * refresh the index with finding a resource

--- pkg/helm/client_test.go
+++ pkg/helm/client_test.go
@@ -1,6 +1,7 @@
 package helm_test
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/homedepot/go-clouddriver/pkg/helm"
@@ -136,8 +137,59 @@ entries:
 	})
 
 	Describe("#GetChart", func() {
+		var name string
+
+		BeforeEach(func() {
+			name = "hello-app"
+			server.AppendHandlers(ghttp.CombineHandlers(
+				ghttp.VerifyRequest(http.MethodGet, "/index.yaml"),
+				ghttp.RespondWith(http.StatusOK, fmt.Sprintf(`apiVersion: v1
+entries:
+  hello-app:
+  - apiVersion: v2
+    appVersion: "1.0"
+    created: 2020-03-20T13:13:42.956Z
+    description: A Helm chart for deploying hello-app to Kuberntes
+    digest: a934a39ba7f77e8e16b609d01c493d7ead5ec24c78d2918d3a00ebaa3c3fdfd2
+    home: https://pages.github.com/test
+    maintainers:
+    - email: me@test.com
+      name: CD Team
+    name: hello-app
+    urls:
+    - %s/artifactory/helm/hello-app-1.0.0.tgz
+    version: 1.0.0
+  invalid-url-chart:
+  - apiVersion: v2
+    appVersion: "1.0"
+    created: 2020-03-20T13:13:42.956Z
+    description: A Helm chart for deploying hello-app to Kuberntes
+    digest: a934a39ba7f77e8e16b609d01c493d7ead5ec24c78d2918d3a00ebaa3c3fdfd2
+    home: https://pages.github.com/test
+    maintainers:
+    - email: me@test.com
+      name: CD Team
+    name: hello-app
+    urls:
+    - haha
+    version: 1.0.0
+  no-urls-chart:
+  - apiVersion: v2
+    appVersion: "2.0"
+    created: 2020-08-14T22:11:19.894Z
+    description: A Helm chart for deploying hello-app to Kubernetes
+    digest: c9c2663958ffdf2c790ebfd0f502176c3ac663014e9d6ce49955c4178c2f43d7
+    home: https://pages.github.com/test
+    maintainers:
+    - email: me@test.com
+      name: CD Team
+    name: no-urls-chart
+    version: 1.0.0`, server.URL())),
+			))
+		})
+
 		JustBeforeEach(func() {
-			b, err = client.GetChart("test-name", "0.1.0")
+			b, err = client.GetChart(name, "1.0.0")
 		})
 
 		When("the uri is invalid", func() {
@@ -147,6 +199,40 @@ entries:
 
 			It("returns an error", func() {
 				Expect(err).ToNot(BeNil())
+				Expect(err.Error()).To(Equal("helm: unable to find chart hello-app-1.0.0: parse \"::haha/index.yaml\": missing protocol scheme"))
+			})
+		})
+
+		When("the chart does not exist", func() {
+			BeforeEach(func() {
+				name = "invalid-chart"
+			})
+
+			It("returns an error", func() {
+				Expect(err).ToNot(BeNil())
+				Expect(err.Error()).To(Equal("helm: unable to find chart invalid-chart-1.0.0: unable to find resource"))
+			})
+		})
+
+		When("the chart has no associated urls", func() {
+			BeforeEach(func() {
+				name = "no-urls-chart"
+			})
+
+			It("returns an error", func() {
+				Expect(err).ToNot(BeNil())
+				Expect(err.Error()).To(Equal("helm: no resource urls defined for chart no-urls-chart-1.0.0"))
+			})
+		})
+
+		When("the chart url is invalid", func() {
+			BeforeEach(func() {
+				name = "invalid-url-chart"
+			})
+
+			It("returns an error", func() {
+				Expect(err).ToNot(BeNil())
+				Expect(err.Error()).To(Equal("Get \"haha\": unsupported protocol scheme \"\""))
 			})
 		})
 
@@ -169,14 +255,14 @@ entries:
 
 			It("returns an error", func() {
 				Expect(err).ToNot(BeNil())
-				Expect(err.Error()).To(Equal("error getting helm chart: 500 Internal Server Error"))
+				Expect(err.Error()).To(Equal("helm: error getting chart: 500 Internal Server Error"))
 			})
 		})
 
 		When("it succeeds", func() {
 			BeforeEach(func() {
 				server.AppendHandlers(ghttp.CombineHandlers(
-					ghttp.VerifyRequest(http.MethodGet, "/test-name-0.1.0.tgz"),
+					ghttp.VerifyRequest(http.MethodGet, "/artifactory/helm/hello-app-1.0.0.tgz"),
 					ghttp.RespondWith(http.StatusOK, `some-binary-data`),
 				))
 			})
