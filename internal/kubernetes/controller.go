9e5a5f145175f79d771f1ef41e920b43a36cfec9
Billy Bolton <billy@bolton.email>
2021-12-10 11:12:34 -0500

    add memcache and update controller to use memcache

--- internal/kubernetes/controller.go
+++ internal/kubernetes/controller.go
@@ -4,9 +4,12 @@ import (
 	"path/filepath"
 	"regexp"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/homedepot/go-clouddriver/internal/kubernetes/cached/disk"
+	"k8s.io/client-go/discovery"
+	"k8s.io/client-go/discovery/cached/memory"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
@@ -32,7 +35,7 @@ type controller struct{}
 // disk cache directory of /var/kube/cache. This is where the client
 // stores and references its discovery of the Kubernetes API server.
 func (c *controller) NewClient(config *rest.Config) (Client, error) {
-	return newClientWithDefaultDiskCache(config)
+	return newClientWithMemoryCache(config)
 }
 
 // NewClientset returns a new kubernetes Clientset wrapper.
@@ -53,6 +56,31 @@ const (
 	defaultTimeout = 180 * time.Second
 	ttl            = 10 * time.Minute
 )
+
+func newClientWithMemoryCache(config *rest.Config) (Client, error) {
+	// If the timeout is not set, set it to the default timeout.
+	if config.Timeout == 0 {
+		config.Timeout = defaultTimeout
+	}
+
+	dynamicClient, err := dynamic.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	mapper, err := mapperForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	kubeClient := &client{
+		c:      dynamicClient,
+		config: config,
+		mapper: mapper,
+	}
+
+	return kubeClient, nil
+}
 
 func newClientWithDefaultDiskCache(config *rest.Config) (Client, error) {
 	// If the timeout is not set, set it to the default timeout.
@@ -77,7 +105,6 @@ func newClientWithDefaultDiskCache(config *rest.Config) (Client, error) {
 	}
 
 	mapper := restmapper.NewDeferredDiscoveryRESTMapper(cdc)
-
 	kubeClient := &client{
 		c:      dynamicClient,
 		config: config,
@@ -85,6 +112,68 @@ func newClientWithDefaultDiskCache(config *rest.Config) (Client, error) {
 	}
 
 	return kubeClient, nil
+}
+
+var (
+	mux           sync.Mutex
+	cachedConfigs = map[string]*rest.Config{}
+	cachedMappers = map[string]*restmapper.DeferredDiscoveryRESTMapper{}
+)
+
+func mapperForConfig(inConfig *rest.Config) (*restmapper.DeferredDiscoveryRESTMapper, error) {
+	config := inConfig
+
+	if _, ok := cachedConfigs[config.Host]; ok {
+		cachedConfig := cachedConfigs[config.Host]
+		if string(cachedConfig.TLSClientConfig.CAData) != string(config.TLSClientConfig.CAData) ||
+			cachedConfig.BearerToken != config.BearerToken {
+			err := setCaches(config)
+			if err != nil {
+				return nil, err
+			}
+		}
+	} else {
+		err := setCaches(config)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return cachedMapper(config), nil
+}
+
+func setCaches(config *rest.Config) error {
+	m, err := newMapperForConfig(config)
+	if err != nil {
+		return err
+	}
+
+	mux.Lock()
+	defer mux.Unlock()
+
+	cachedConfigs[config.Host] = config
+	cachedMappers[config.Host] = m
+
+	return nil
+}
+
+func newMapperForConfig(config *rest.Config) (*restmapper.DeferredDiscoveryRESTMapper, error) {
+	// DiscoveryClient queries API server about the resources
+	dc, err := discovery.NewDiscoveryClientForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	mapper := restmapper.NewDeferredDiscoveryRESTMapper(memory.NewMemCacheClient(dc))
+
+	return mapper, nil
+}
+
+func cachedMapper(config *rest.Config) *restmapper.DeferredDiscoveryRESTMapper {
+	mux.Lock()
+	defer mux.Unlock()
+
+	return cachedMappers[config.Host]
 }
 
 // overlyCautiousIllegalFileCharacters matches characters that *might* not be supported.
