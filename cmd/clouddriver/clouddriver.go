6e6ead43652203c0a96d8ec8c486e56754975261
Victor <94372428+victor-homedepot@users.noreply.github.com>
2022-05-10 14:14:56 -0400

    Update clouddriver.go
    Declare const for arcade token expiration

--- cmd/clouddriver/clouddriver.go
+++ cmd/clouddriver/clouddriver.go
@@ -22,7 +22,8 @@ import (
 )
 
 const (
-	mysqlDefaultStringSize = 256
+	arcadeShortExpirationSeconds = 60
+	mysqlDefaultStringSize       = 256
 )
 
 var (
@@ -67,7 +68,7 @@ func init() {
 	}
 
 	arcadeClient.WithAPIKey(arcadeAPIKey)
-	arcadeClient.WithShortExpiration(60)
+	arcadeClient.WithShortExpiration(arcadeShortExpirationSeconds)
 
 	ic := &internal.Controller{
 		ArcadeClient:                  arcadeClient,
